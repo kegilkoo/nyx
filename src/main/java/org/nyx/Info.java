d80e4742ae1e69bcde59603eb279460219831ce7
kegilko <nomads_kei@hotmail.com>
2015-06-12 01:47:39 +0200

    je glisse une erreur dans le code java, point virgule manquant

--- src/main/java/org/nyx/Info.java
+++ src/main/java/org/nyx/Info.java
@@ -67,7 +67,7 @@ public class Info {
 	    if (bytes < unit) return bytes + " B";
 	    int exp = (int) (Math.log(bytes) / Math.log(unit));
 	    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
-	    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+	    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre)
 	}
 
 	@Override
