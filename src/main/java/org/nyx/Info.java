c3d84ef66e3167f55a4a5aab68a8c7968c6bb18d
kegilko <nomads_kei@hotmail.com>
2015-06-18 22:58:11 +0200

    change info.java : test auto build

--- src/main/java/org/nyx/Info.java
+++ src/main/java/org/nyx/Info.java
@@ -72,7 +72,7 @@ public class Info {
 
 	@Override
 	public String toString() {
-		return "Les iiiPLPLnfos : " +
+		return "Les infos : " +
 				"<br/>userName=" + getUserName() 
 			  + "<br/>diskSize=" + humanReadableByteCount(getDiskSize(),false)
 			  + "<br/>maxMemory=" + humanReadableByteCount(getMaxMemory(),false) 
